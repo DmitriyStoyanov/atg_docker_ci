8d4271a6fd7a137dc82f38e8baf7bf4458bf7763
Dmitriy Stoyanov <dmitry_stoyanov@epam.com>
2020-01-21 18:32:55 +0300

    Removed Dockerfile_ci-jdk8

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 docker build --rm -t local/atg_docker_ci:ci -f Dockerfile .
 docker build --rm -t local/atg_docker_ci:ci-shaper -f Dockerfile_shaper .
-docker build --rm -t local/atg_docker_ci:ci-jdk8 -f Dockerfile_ci-jdk8 .
